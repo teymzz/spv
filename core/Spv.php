86c1ce7a505c73d702ded6fd553b23fb9e10fdeb
Akinola Saheed <teymss@gmail.com>
2023-03-09 02:14:42 +0100

    Testing app

---
+++ core/Spv.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spv\mi\core;
+use spv\mi\core\classes\FileManager;
+
+class Spv {
+
+    static function install(){
+
+        $FileManager = new FileManager;
+
+        $FileManager->setUrl('vendor/spv/mi/');
+
+        if($FileManager->moveContentsTo(__DIR__, ['vendor'])){
+
+            //$FileManager->deleteFile('vendor/');
+            shell_exec('composer dump-autoload -o');
+            //delete the vendor file
+
+        }
+
+    }
+
+}
